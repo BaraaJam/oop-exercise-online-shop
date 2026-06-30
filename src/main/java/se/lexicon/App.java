e6fc7c6a3c56e56d5a5badb2c41d64a1829f6354
Baraa Jamos <Baraajamos@icloud.com>
2026-06-30 09:43:08 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,8 @@
+package se.lexicon;
+
+public class App {
+    static void main() {
+        IO.println(String.format("Hello and welcome!"));
+
+    }
+}
---
+++ src/main/java/se/lexicon/Product.java
@@ -0,0 +1,39 @@
+package se.lexicon;
+
+public class Product {
+    private String name;
+    private double price;
+
+    public Product (String name, double price){
+        if (name == null || name.trim().isBlank())
+            throw new IllegalArgumentException("Product name cannot be empty.");
+
+        if (price < 0)
+            throw new IllegalArgumentException("Price cannot be negative.");
+
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void applyDiscount(double precent) {
+        if (precent > 80.0)
+            throw new IllegalArgumentException("Discount cannot be greater than 80%.");
+
+        if (precent < 0)
+            throw new IllegalArgumentException("Discount cannot be negative.");
+
+        this.price = this.price * (1 - (precent / 100));
+    }
+
+    public void printInfo(){
+        System.out.printf("Product: %s | Price: $%.2f%n", this.name, this.price);
+    }
+}
